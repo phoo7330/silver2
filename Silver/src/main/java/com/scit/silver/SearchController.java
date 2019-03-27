a51ec1778fb9164a7f2a12034b7c3273e4c5a031
Danei <951753pm@naver.com>
2019-03-27 10:33:15 +0900

    최신

--- Silver/src/main/java/com/scit/silver/SearchController.java
+++ Silver/src/main/java/com/scit/silver/SearchController.java
@@ -92,7 +92,7 @@ public class SearchController {
 	public String searchDetail(int seach_seq) {
 
 		System.out.println(seach_seq);
-		//오늘 할것,코딩하고싶다111116665
+		//오늘 할것,코딩하고싶다11111666577
 		return "redirect:/search"; 
 	}
 }
