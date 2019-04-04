1cea1554c63c818e665119b9548e8d4988d0f8b6
Danei <951753pm@naver.com>
2019-04-04 10:28:15 +0900

    vo)SeniorCitizen,SeniorCitizenDetails추가

---
+++ Silver/src/main/java/com/scit/silver/vo/SeniorCitizenDetails.java
@@ -0,0 +1,237 @@
+package com.scit.silver.vo;
+
+public class SeniorCitizenDetails {
+	private int scd_seq;
+	private int sc_seq;
+	private String scd_addressa;
+	private String scd_addressb;
+	private String scd_addressc;
+	private int scd_tall;
+	private int scd_kg;
+	private int scd_silvernum;
+	private String scd_grade;
+	private String scd_disease;
+	private String scd_meal;
+	private String scd_care;
+	private String scd_bath;
+	private String scd_toilet;
+	private String scd_favorite;
+	private String scd_least;
+	private String scd_allergy;
+	private String scd_diabetes;
+	private String scd_practice;
+	private String scd_propensity;
+	private String scd_language;
+	
+	public SeniorCitizenDetails(int scd_seq, int sc_seq, String scd_addressa, String scd_addressb, String scd_addressc,
+			int scd_tall, int scd_kg, int scd_silvernum, String scd_grade, String scd_disease, String scd_meal,
+			String scd_care, String scd_bath, String scd_toilet, String scd_favorite, String scd_least,
+			String scd_allergy, String scd_diabetes, String scd_practice, String scd_propensity, String scd_language) {
+		super();
+		this.scd_seq = scd_seq;
+		this.sc_seq = sc_seq;
+		this.scd_addressa = scd_addressa;
+		this.scd_addressb = scd_addressb;
+		this.scd_addressc = scd_addressc;
+		this.scd_tall = scd_tall;
+		this.scd_kg = scd_kg;
+		this.scd_silvernum = scd_silvernum;
+		this.scd_grade = scd_grade;
+		this.scd_disease = scd_disease;
+		this.scd_meal = scd_meal;
+		this.scd_care = scd_care;
+		this.scd_bath = scd_bath;
+		this.scd_toilet = scd_toilet;
+		this.scd_favorite = scd_favorite;
+		this.scd_least = scd_least;
+		this.scd_allergy = scd_allergy;
+		this.scd_diabetes = scd_diabetes;
+		this.scd_practice = scd_practice;
+		this.scd_propensity = scd_propensity;
+		this.scd_language = scd_language;
+	}
+
+	public SeniorCitizenDetails() {
+		super();
+	}
+
+	public int getScd_seq() {
+		return scd_seq;
+	}
+
+	public void setScd_seq(int scd_seq) {
+		this.scd_seq = scd_seq;
+	}
+
+	public int getSc_seq() {
+		return sc_seq;
+	}
+
+	public void setSc_seq(int sc_seq) {
+		this.sc_seq = sc_seq;
+	}
+
+	public String getScd_addressa() {
+		return scd_addressa;
+	}
+
+	public void setScd_addressa(String scd_addressa) {
+		this.scd_addressa = scd_addressa;
+	}
+
+	public String getScd_addressb() {
+		return scd_addressb;
+	}
+
+	public void setScd_addressb(String scd_addressb) {
+		this.scd_addressb = scd_addressb;
+	}
+
+	public String getScd_addressc() {
+		return scd_addressc;
+	}
+
+	public void setScd_addressc(String scd_addressc) {
+		this.scd_addressc = scd_addressc;
+	}
+
+	public int getScd_tall() {
+		return scd_tall;
+	}
+
+	public void setScd_tall(int scd_tall) {
+		this.scd_tall = scd_tall;
+	}
+
+	public int getScd_kg() {
+		return scd_kg;
+	}
+
+	public void setScd_kg(int scd_kg) {
+		this.scd_kg = scd_kg;
+	}
+
+	public int getScd_silvernum() {
+		return scd_silvernum;
+	}
+
+	public void setScd_silvernum(int scd_silvernum) {
+		this.scd_silvernum = scd_silvernum;
+	}
+
+	public String getScd_grade() {
+		return scd_grade;
+	}
+
+	public void setScd_grade(String scd_grade) {
+		this.scd_grade = scd_grade;
+	}
+
+	public String getScd_disease() {
+		return scd_disease;
+	}
+
+	public void setScd_disease(String scd_disease) {
+		this.scd_disease = scd_disease;
+	}
+
+	public String getScd_meal() {
+		return scd_meal;
+	}
+
+	public void setScd_meal(String scd_meal) {
+		this.scd_meal = scd_meal;
+	}
+
+	public String getScd_care() {
+		return scd_care;
+	}
+
+	public void setScd_care(String scd_care) {
+		this.scd_care = scd_care;
+	}
+
+	public String getScd_bath() {
+		return scd_bath;
+	}
+
+	public void setScd_bath(String scd_bath) {
+		this.scd_bath = scd_bath;
+	}
+
+	public String getScd_toilet() {
+		return scd_toilet;
+	}
+
+	public void setScd_toilet(String scd_toilet) {
+		this.scd_toilet = scd_toilet;
+	}
+
+	public String getScd_favorite() {
+		return scd_favorite;
+	}
+
+	public void setScd_favorite(String scd_favorite) {
+		this.scd_favorite = scd_favorite;
+	}
+
+	public String getScd_least() {
+		return scd_least;
+	}
+
+	public void setScd_least(String scd_least) {
+		this.scd_least = scd_least;
+	}
+
+	public String getScd_allergy() {
+		return scd_allergy;
+	}
+
+	public void setScd_allergy(String scd_allergy) {
+		this.scd_allergy = scd_allergy;
+	}
+
+	public String getScd_diabetes() {
+		return scd_diabetes;
+	}
+
+	public void setScd_diabetes(String scd_diabetes) {
+		this.scd_diabetes = scd_diabetes;
+	}
+
+	public String getScd_practice() {
+		return scd_practice;
+	}
+
+	public void setScd_practice(String scd_practice) {
+		this.scd_practice = scd_practice;
+	}
+
+	public String getScd_propensity() {
+		return scd_propensity;
+	}
+
+	public void setScd_propensity(String scd_propensity) {
+		this.scd_propensity = scd_propensity;
+	}
+
+	public String getScd_language() {
+		return scd_language;
+	}
+
+	public void setScd_language(String scd_language) {
+		this.scd_language = scd_language;
+	}
+
+	@Override
+	public String toString() {
+		return "SeniorCitizenDetails [scd_seq=" + scd_seq + ", sc_seq=" + sc_seq + ", scd_addressa=" + scd_addressa
+				+ ", scd_addressb=" + scd_addressb + ", scd_addressc=" + scd_addressc + ", scd_tall=" + scd_tall
+				+ ", scd_kg=" + scd_kg + ", scd_silvernum=" + scd_silvernum + ", scd_grade=" + scd_grade
+				+ ", scd_disease=" + scd_disease + ", scd_meal=" + scd_meal + ", scd_care=" + scd_care + ", scd_bath="
+				+ scd_bath + ", scd_toilet=" + scd_toilet + ", scd_favorite=" + scd_favorite + ", scd_least="
+				+ scd_least + ", scd_allergy=" + scd_allergy + ", scd_diabetes=" + scd_diabetes + ", scd_practice="
+				+ scd_practice + ", scd_propensity=" + scd_propensity + ", scd_language=" + scd_language + "]";
+	}
+	
+}
